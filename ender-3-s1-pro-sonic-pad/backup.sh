fe728abd68911d7e6fe400fc364b8af7e9316c7e
Gabriel Saraiva <extremez3r0@gmail.com>
2024-05-03 20:31:19 -0300

    2024-05-03 - Pre Changes

--- ender-3-s1-pro-sonic-pad/backup.sh
+++ ender-3-s1-pro-sonic-pad/backup.sh
@@ -3,11 +3,11 @@
 set -e
 
 echo "Copying files"
-scp root@192.168.68.57:/mnt/UDISK/printer_config3/printer.cfg .
-scp root@192.168.68.57:/mnt/UDISK/printer_config3/macros.cfg .
-scp root@192.168.68.57:/mnt/UDISK/printer_config3/moonraker.conf .
-scp root@192.168.68.57:/mnt/UDISK/printer_config3/cx_printer.cfg .
-scp root@192.168.68.57:/mnt/UDISK/printer_config3/cx_gmcro.cfg .
+scp -O root@192.168.68.57:/mnt/UDISK/printer_config3/printer.cfg .
+scp -O root@192.168.68.57:/mnt/UDISK/printer_config3/macros.cfg .
+scp -O root@192.168.68.57:/mnt/UDISK/printer_config3/moonraker.conf .
+scp -O root@192.168.68.57:/mnt/UDISK/printer_config3/cx_printer.cfg .
+scp -O root@192.168.68.57:/mnt/UDISK/printer_config3/cx_gmcro.cfg .
 
 echo "Commiting files"
 git add -A
